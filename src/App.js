4146a8e2e9fe575868147b6684444d7a04a51081
jingyaopu <jingyaopu@gmail.com>
2019-11-15 22:41:30 -0800

    branch master IOO# Your branch is up to date with 'origin/master'.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState,useContext}from 'react';
+import {themes,ThemeContext} from "./Theme";
+//import themeButton from "./changeButton";
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function ThemeButton(props){
+  let context = useContext(ThemeContext);
+  return(
+      <button {...props} style = {context}>button</button>
   );
 }
-
-export default App;
+ThemeButton.contextType = ThemeContext
+export default function App() {
+  const [theme,setTheme] = useState(themes.dark);
+  //
+  const changeTheme = ()=> setTheme(theme === themes.light? themes.dark:themes.light);
+  return (
+    <div>
+    <ThemeContext.Provider value={theme}>
+      <ThemeButton onClick={changeTheme}/>
+    </ThemeContext.Provider>
+    <ThemeContext.Provider value={theme === themes.light? themes.dark:themes.light}>
+      <ThemeButton onClick={changeTheme}/>
+    </ThemeContext.Provider>
+    
+    </div>
+  )
+}
